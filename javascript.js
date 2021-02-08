981b427d6807bdac8e989483a93f4a55aa7750f8
GeorgeBaras <georgebaras@outlook.com>
2021-02-08 09:52:45 +0200

    Adding a couple of fields to send data from html to javascript and vice versa

--- javascript.js
+++ javascript.js
@@ -1,2 +1,33 @@
-console.log("I am in your javascript file");
-console.log("Another meaningless log");
+
+let dog0 = {   // <<< is this the right way?
+    name:"",
+    breed:""
+};
+
+let dogs = [];
+
+function createDog(){
+    // Got the input from html
+    let nameFromInput  = document.getElementById("dogName").value;
+    let breedFromInput = document.getElementById("dogBreed").value;
+    // Created a new dog
+    let newDog = Object.create(dog0);
+    newDog.name = nameFromInput;
+    newDog.breed = breedFromInput;
+    // added the new dog to the list
+    dogs.push(newDog);
+} 
+
+// The following needs some fixing
+function showDogs(){
+    for(var i=0; i<dogs.length; i++){
+        let dog = dogs[i].name+" of breed:"+dogs[i].breed;
+        var node = document.createElement("LI"); // <li></li>
+        var textnode = document.createTextNode(dog); // "Stark of breed: Tskyli"
+        node.appendChild(textnode);
+        document.getElementById("dogs").appendChild(node);
+    
+    }
+
+}
+
